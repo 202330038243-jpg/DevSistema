8b2e9b0910f99eda0d196e5a0900a3ff73a133b4
202330038243-jpg <202330038243@estudante.rn.gov.br>
2025-11-29 23:08:11 -0300

    Atualizar o funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -1,1 +1,14 @@
-body
+document.getElementById("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const senha = document.getElementById("senha").value;
+    const email = document.getElementById("email").value;
+
+    if (!nome || !senha || !email) {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    alert("Login realizado com sucesso!");
+});
